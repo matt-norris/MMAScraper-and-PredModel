5d1b3973653da920657d19b31f5d17ac27a44120
matt <norrism7@msu.edu>
2023-05-12 16:18:14 -0400

    reinstated driver in scraper

--- MMAScraper/PredictionModel.py
+++ MMAScraper/PredictionModel.py
@@ -1,19 +1,28 @@
 import tkinter as tk
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.feature_selection import RFE
 from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+
 # Load data from CSV file
 df = pd.read_csv('data.csv')
 
-# Clean and preprocess data as needed
-# For example, drop irrelevant columns
+# Add new features
+df['win_ratio_diff'] = (df['B_wins'] / (df['B_wins'] + df['B_losses'])) - (
+            df['R_wins'] / (df['R_wins'] + df['R_losses']))
+df['form_diff'] = df['B_current_win_streak'] - df['R_current_win_streak']
+
+# Update relevant_columns list with new features
 relevant_columns = [
     'R_fighter', 'B_fighter', 'Winner', 'B_avg_KD', 'B_avg_opp_KD', 'B_avg_SIG_STR_pct', 'B_avg_opp_SIG_STR_pct',
     'R_avg_KD', 'R_avg_opp_KD', 'R_avg_SIG_STR_pct', 'R_avg_opp_SIG_STR_pct', 'B_age', 'R_age', 'B_Height_cms',
-    'R_Height_cms', 'B_Reach_cms', 'R_Reach_cms', 'B_total_rounds_fought', 'R_total_rounds_fought'
+    'R_Height_cms', 'B_Reach_cms', 'R_Reach_cms', 'B_total_rounds_fought', 'R_total_rounds_fought',
+    'win_ratio_diff', 'form_diff'
 ]
+
 df = df[relevant_columns]
 
 # Drop rows with missing values if applicable
@@ -29,9 +38,20 @@ df['height_diff'] = df['B_Height_cms'] - df['R_Height_cms']
 df['reach_diff'] = df['B_Reach_cms'] - df['R_Reach_cms']
 df['total_rounds_fought_diff'] = df['B_total_rounds_fought'] - df['R_total_rounds_fought']
 
+# Define feature columns
+feature_columns = [
+    'KD_diff', 'opp_KD_diff', 'SIG_STR_pct_diff', 'opp_SIG_STR_pct_diff',
+    'age_diff', 'height_diff', 'reach_diff', 'total_rounds_fought_diff',
+]
+
+# Split data into features (X) and target (y) variables
+# Convert string class labels to numeric values
+le = LabelEncoder()
+df['Winner'] = le.fit_transform(df['Winner'])
+
 # Split data into features (X) and target (y) variables
 X = df[['KD_diff', 'opp_KD_diff', 'SIG_STR_pct_diff', 'opp_SIG_STR_pct_diff',
-         'age_diff', 'height_diff', 'reach_diff', 'total_rounds_fought_diff']]
+        'age_diff', 'height_diff', 'reach_diff', 'total_rounds_fought_diff']]
 y = df['Winner']
 
 # Split data into training and testing sets
@@ -42,33 +62,44 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Train a Random Forest model
+# Perform feature selection using Recursive Feature Elimination (RFE)
+model = XGBClassifier(random_state=42)
+rfe = RFE(estimator=model, n_features_to_select=8)
+rfe.fit(X_train, y_train)
 
-from sklearn.model_selection import GridSearchCV
+# Transform X_train and X_test based on the selected features
+X_train_rfe = rfe.transform(X_train)
+X_test_rfe = rfe.transform(X_test)
 
-# Define the hyperparameter grid
-param_grid = {
-    'n_estimators': [100, 200, 300, 400, 500],
-    'max_depth': [None, 10, 20, 30, 40, 50],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4]
+# Train an XGBoost model with hyperparameter tuning using RandomizedSearchCV
+param_dist = {
+    'n_estimators': np.arange(100, 1001, 100),
+    'learning_rate': np.logspace(-3, 0, 10),
+    'max_depth': np.arange(1, 11),
+    'min_child_weight': np.arange(1, 11),
+    'gamma': np.linspace(0, 1, 11),
+    'subsample': np.linspace(0.5, 1, 6),
+    'colsample_bytree': np.linspace(0.5, 1, 6)
 }
 
-# Instantiate the RandomForestClassifier
-model = RandomForestClassifier(random_state=42)
+model = XGBClassifier(random_state=42)
 
-# Instantiate the GridSearchCV with the model and hyperparameter grid
-grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+random_search = RandomizedSearchCV(
+    estimator=model, param_distributions=param_dist, n_iter=100, cv=5,
+    scoring='accuracy', n_jobs=-1, verbose=2, random_state=42
+)
 
-# Fit the grid_search to the data
-grid_search.fit(X_train, y_train)
+random_search.fit(X_train_rfe, y_train)
 
 # Get the best model
-best_model = grid_search.best_estimator_
+best_model = random_search.best_estimator_
 
 # Evaluate the best model on the test set
-y_pred = best_model.predict(X_test)
+y_pred = best_model.predict(X_test_rfe)
 accuracy = accuracy_score(y_test, y_pred)
+
+# Get the names of the selected features
+selected_features = np.array(feature_columns)[rfe.support_]
 
 
 def get_prediction():
@@ -81,18 +112,10 @@ def get_prediction():
 
     if not blue_fighter_data.empty and not red_fighter_data.empty:
         # Calculate feature differences for user input
-        KD_diff = blue_fighter_data['KD_diff'].values[0]
-        opp_KD_diff = blue_fighter_data['opp_KD_diff'].values[0]
-        SIG_STR_pct_diff = blue_fighter_data['SIG_STR_pct_diff'].values[0]
-        opp_SIG_STR_pct_diff = blue_fighter_data['opp_SIG_STR_pct_diff'].values[0]
-        age_diff = blue_fighter_data['B_age'].values[0] - red_fighter_data['R_age'].values[0]
-        height_diff = blue_fighter_data['B_Height_cms'].values[0] - red_fighter_data['R_Height_cms'].values[0]
-        reach_diff = blue_fighter_data['B_Reach_cms'].values[0] - red_fighter_data['R_Reach_cms'].values[0]
-        total_rounds_fought_diff = blue_fighter_data['B_total_rounds_fought'].values[0] - red_fighter_data['R_total_rounds_fought'].values[0]
+        input_data = blue_fighter_data[feature_columns].iloc[0] - red_fighter_data[feature_columns].iloc[0]
 
         # Create input array and standardize the features
-        input_array = [[KD_diff, opp_KD_diff, SIG_STR_pct_diff, opp_SIG_STR_pct_diff,
-                        age_diff, height_diff, reach_diff, total_rounds_fought_diff]]
+        input_array = input_data[selected_features].values.reshape(1, -1)
         input_array = scaler.transform(input_array)
 
         # Make prediction
@@ -104,10 +127,12 @@ def get_prediction():
         prediction_probability = prediction_proba[0][prediction_index]
 
         # Update the prediction_label text
-        prediction_label.config(text=f"Winner: {prediction[0]} (Probability: {prediction_probability:.2%})")
+        prediction_label.config(
+            text=f"Winner: {le.inverse_transform(prediction)[0]} (Probability: {prediction_probability:.2%})")
 
     else:
         prediction_label.config(text="Error: One or both fighters not found in the dataset.")
+
 
 # Create tkinter window
 root = tk.Tk()
@@ -123,14 +148,12 @@ red_fighter_label.grid(row=1, column=0, padx=10, pady=10)
 prediction_label = tk.Label(root, text="")
 prediction_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-# Create entry boxes
 blue_fighter_entry = tk.Entry(root)
 blue_fighter_entry.grid(row=0, column=1, padx=10, pady=10)
 
 red_fighter_entry = tk.Entry(root)
 red_fighter_entry.grid(row=1, column=1, padx=10, pady=10)
 
-# Create button
 predict_button = tk.Button(root, text="Predict", command=get_prediction)
 predict_button.grid(row=2, column=1, padx=10, pady=10)
 
